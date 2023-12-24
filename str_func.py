60bb2a7266af4e08580d7cd2ef77ac134de57b58
Artem <baza.a1@bk.ru>
2023-12-24 23:14:12 +0300

    докстринги и добавление get title

--- str_func.py
+++ str_func.py
@@ -1,8 +1,20 @@
 def with_a_capital_letter(text):
     """
-    Выводит текст
+    получает текст и выводит всё
+    с заглавной
     """
     print(text.upper())
 
 
 with_a_capital_letter("блин-блинский")
+
+
+def get_title(text):
+    """
+    получает текст и выводит каждое
+    слово с заглавной буквы
+    """
+    print(text.title())
+
+
+get_title("ёлки-иголки")
